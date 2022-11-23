3cd41e5bea019306fe452beb059b73ec2b57fe72
Joel bosio <bosio.joel@volta.ts.it>
2022-11-23 10:44:47 +0100

    added form

--- index.php
+++ index.php
@@ -1,2 +1,16 @@
+<html>
+
+<head>
+</head>
+
+<body>
+    <form>
+        <input name="key" type="number" />
+        <textarea name="inputText" placeholder="inserire testo da cifrare"/>
+    </form>
+</body>
+
+</html>
 <?php
+
 ?>
